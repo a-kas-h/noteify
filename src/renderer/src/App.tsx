ffb1b044aeac9ea117fb6b0bc5b4fd471f729186
a-kas-h <akashsaminathan24@gmail.com>
2025-07-12 21:19:07 +0530

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,15 @@
+import { Content, DraggableTopBar, RootLayout, Sidebar } from "./components"
+
+const App = () => {
+  return(
+    <>
+    <DraggableTopBar/>
+    <RootLayout>
+      <Sidebar className = "p-2">Sidebar</Sidebar>
+      <Content className="border-l bg-zinc-900/50 border-l-white/50">Content</Content>
+    </RootLayout>
+    </>
+  ) 
+}
+
+export default App
